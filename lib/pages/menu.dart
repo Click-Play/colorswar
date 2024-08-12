72fad505f1b0076408a0f1150574a6ae3651edbd
Alexis GONNEAUD <alexis.gonneaud@hotmail.fr>
2024-08-12 18:16:27 +0200

    feat: Add audio player for menu opening sound

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class MenuPage extends StatelessWidget {
+class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
+
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  late AudioPlayer _audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+
+    // Start the player as soon as the app is displayed.
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await _audioPlayer.setSource(AssetSource('Menu_opening.mp3'));
+      await _audioPlayer.resume();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Release all sources and dispose the player.
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
